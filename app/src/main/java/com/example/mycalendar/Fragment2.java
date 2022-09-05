0aed91324325b722b85787925ffca602c2ac7281
user <ed9398.www@daum.net>
2022-09-05 10:10:11 +0900

    0905 image setscale

--- app/src/main/java/com/example/mycalendar/Fragment2.java
+++ app/src/main/java/com/example/mycalendar/Fragment2.java
@@ -7,58 +7,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Fragment2 extends Fragment {
+public class Fragment2 extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment2() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        Button btn_s = view.findViewById(R.id.btn_small);
+        Button btn_l = view.findViewById(R.id.btn_large);
+        imageView = view.findViewById(R.id.image_main);
+
+        btn_s.setOnClickListener(this);
+        btn_l.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_small:
+                imageView.setScaleX(imageView.getScaleX()-0.1f);
+                imageView.setScaleY(imageView.getScaleY()-0.1f);
+                break;
+
+            case R.id.btn_large:
+                imageView.setScaleX(imageView.getScaleX()+0.1f);
+                imageView.setScaleY(imageView.getScaleY()+0.1f);
+                break;
+
+        }
     }
 }
